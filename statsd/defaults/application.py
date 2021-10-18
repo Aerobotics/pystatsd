773719983a18248b448ab77092a8bb4ba8ffc369
SenYeezus <simpsonsenyo@gmail.com>
2021-10-18 09:55:41 +0200

    feat: sets application name

--- statsd/defaults/application.py
+++ statsd/defaults/application.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import os
+import warnings
 
 from statsd import defaults
 from statsd.client import StatsClient
@@ -9,13 +10,25 @@ STATSD_PREFIX = "aerobotics.application"
 statsd = None
 
 if statsd is None:
-    host = os.getenv('STATSD_HOST', defaults.HOST)
-    port = int(os.getenv('STATSD_PORT', defaults.PORT))
+    host = os.getenv("STATSD_HOST", defaults.HOST)
+    port = int(os.getenv("STATSD_PORT", defaults.PORT))
     prefix = STATSD_PREFIX
-    maxudpsize = int(os.getenv('STATSD_MAXUDPSIZE', defaults.MAXUDPSIZE))
+    maxudpsize = int(os.getenv("STATSD_MAXUDPSIZE", defaults.MAXUDPSIZE))
     ipv6 = False
-    statsd = StatsClient(host=host, port=port, prefix=prefix,
-                         maxudpsize=maxudpsize, ipv6=ipv6)
-    
+    statsd = StatsClient(host=host, port=port, prefix=prefix, maxudpsize=maxudpsize, ipv6=ipv6)
+
     environment = os.getenv("ENVIRONMENT_NAME", "dev")
+    application = os.getenv("APPLICATION_NAME")
+
+    tags = {"environment": environment}
+
+    if application is not None:
+        tags["application"] = application
+    else:
+        warnings.warn(
+            "Application name not set. Could not find environment "
+            "variable APPLICATION_NAME. This will make it difficult "
+            "to query for these metrics."
+        )
+
     statsd.bind(tags={"environment": environment})
