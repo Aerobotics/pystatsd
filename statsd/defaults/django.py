46b9e1c4094d751f2888b3fdce7a95a059de9c96
SenYeezus <simpsonsenyo@gmail.com>
2021-10-20 14:18:05 +0200

    improvement: get all variables from environment for Django
    BREAKING CHANGE: fetches statsd configuration from environment instead
    of Django config settings. However, won't actually make a major version
    bump as these settings are set by default already and no one is using
    the Django conf settings

--- statsd/defaults/django.py
+++ statsd/defaults/django.py
@@ -12,9 +12,9 @@ statsd = None
 
 if statsd is None:
     host = os.getenv("STATSD_HOST", defaults.HOST)
-    port = getattr(settings, 'STATSD_PORT', defaults.PORT)
+    port = os.getenv('STATSD_PORT', defaults.PORT)
     prefix = STATSD_PREFIX
-    maxudpsize = getattr(settings, 'STATSD_MAXUDPSIZE', defaults.MAXUDPSIZE)
+    maxudpsize = os.getenv('STATSD_MAXUDPSIZE', defaults.MAXUDPSIZE)
     ipv6 = False
     statsd = StatsClient(host=host, port=port, prefix=prefix,
                          maxudpsize=maxudpsize, ipv6=ipv6)
